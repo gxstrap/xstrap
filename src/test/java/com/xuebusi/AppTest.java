f5b202f864dc13a474ed89bf9f6a2d23acfeca73
xuebusi <xbs1019@126.com>
2017-11-04 17:43:45 +0800

    merge

--- src/test/java/com/xuebusi/AppTest.java
+++ src/test/java/com/xuebusi/AppTest.java
@@ -9,6 +9,6 @@ public class AppTest {
     public static void main(String[] args) {
         String admin = MD5Utils.md5("admin3");
         String pwd = MD5Utils.md5("admin3");
-        System.out.println(admin);
+        System.out.println(pwd);
     }
 }
