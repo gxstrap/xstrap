f84e4b9c899d3b7f01dcab4841a418f4b6c03e38
xuebusi <xbs1019@126.com>
2017-11-06 22:18:24 +0800

    测试@ModelAttribute

--- src/main/java/com/xuebusi/controller/TestController.java
+++ src/main/java/com/xuebusi/controller/TestController.java
@@ -1,6 +1,10 @@
 package com.xuebusi.controller;
 
 import com.xuebusi.common.result.JsonResult;
+import com.xuebusi.entity.LoginInfo;
+import com.xuebusi.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +19,9 @@ import java.util.Map;
 @RequestMapping(value = "/test")
 public class TestController {
 
+    @Autowired
+    private LoginService loginService;
+
     /**
      * 测试
      * @return
@@ -25,4 +32,39 @@ public class TestController {
         map.put("1", System.currentTimeMillis());
         return new JsonResult().JsonSuccess(map);
     }
+
+    /**
+     * 测试@ModelAttribute的使用
+     * 被@ModelAttribute注解修饰的方法就相当于SpringMVC拦截器，
+     * 在调用该类中所有的方法之前都会先调用此方法。
+     * @param loginInfo
+     * @param map
+     */
+    @ModelAttribute
+    public void getLoginInfo(LoginInfo loginInfo, Map<String, Object> map) {
+        if (loginInfo.getId() != null && loginInfo.getId() > 0) {
+            map.put("loginInfo", loginService.findOne(loginInfo.getId()));
+        }
+    }
+
+    /**
+     * 【测试修改登录用户信息】
+     *
+     *  结合上面的@ModelAttribute注解标识的方法，
+     *  先判断LoginInfo的id不为空，说明是修改，
+     *  就根据id从数据库中查询出LoginInfo对象，
+     *  并放到请求域（map）中。
+     *  然后SpringMVC会将表单中的值赋值给查询出来的
+     *  LoginInfo对象并将其传递给此方法的入参中的"loginInfo"，
+     *  最后再进行修改操作，这样才能保证LoginInfo对象中的
+     *  密码属性不会被修改或者赋值为null。
+     * @param loginInfo
+     * @return
+     */
+    @RequestMapping(value = "/saveLoginInfo")
+    public String updateLoginInfo(@ModelAttribute("loginInfo") LoginInfo loginInfo) {
+        loginService.save(loginInfo);
+        return "success";
+    }
+
 }
